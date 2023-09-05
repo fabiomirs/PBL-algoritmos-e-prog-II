a5fdc1658f7a129e838b7571775c4924806c405f
fabiomirs <fabiomirs@users.noreply.github.com>
2023-09-05 07:41:01 -0300

    :construction_worker: Implementação de um teste unitário
    - Essa implementação é apenas para verificar o funcionamento de um teste, não sendo o teste definitivo para a classe Usuário.

---
+++ PBL/testes/model/UsuarioTest.java
@@ -0,0 +1,15 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioTest {
+    @Test
+    public void renovandoEmprestimo() {
+        Livro livro = new Livro("teste","testando",456789,"setor teste","teste","teste","teste");
+        Usuario usuario = new Usuario("Fábio", "teste", 545454);
+        usuario.renovarEmprestimo(livro);
+
+    }
+}
