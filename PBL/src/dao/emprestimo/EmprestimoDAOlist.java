c1a72260be95c7dd50f39c716593a2cb23b9c6aa
fabiomirs <fabiomirs@users.noreply.github.com>
2023-09-11 14:08:51 -0300

    :snowflake: feat: Atualização dos métodos adicionados a classe DAO do Emprestimo.

--- PBL/src/dao/emprestimo/EmprestimoDAOlist.java
+++ PBL/src/dao/emprestimo/EmprestimoDAOlist.java
@@ -3,24 +3,42 @@ package dao.emprestimo;
 import model.Emprestimo;
 import model.Livro;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
-public class EmprestimoDAOlist implements EmprestimoDAO{
-    private List<Emprestimo> reservas;
+public class EmprestimoDAOlist implements EmprestimoDAO {
     private List<Emprestimo> emprestimos;
-
-    public EmprestimoDAOlist(){
+    
+    public EmprestimoDAOlist() {
         this.emprestimos = new ArrayList<>();
-        this.reservas = new ArrayList<>();
     }
+
     @Override
     public Emprestimo create(Emprestimo objeto) {
-        if (objeto.getUsuario().getStatusConta().equals("Bloqueado") || objeto.getLivro().getStatusLivro().equals("Emprestado")){
+        if (objeto.getUsuario().getStatusConta().equals("Bloqueado")) {
             return null;
         }
-        else{
-            objeto.getLivro().setStatusLivro("Emprestado");
+
+        Livro livro = objeto.getLivro();
+
+        if ("Emprestado".equals(livro.getStatusLivro())) {
+            return null;
+        } else if (!objeto.getLivro().getReservas().isEmpty()) {
+            Emprestimo reserva = livro.proxReserva();
+            if (reserva != null && reserva.getUsuario().getNumIdentificacao().equals(objeto.getUsuario().getNumIdentificacao())) {
+                livro.setStatusLivro("Emprestado");
+                this.emprestimos.add(objeto);
+                livro.tiraFilareserva();
+                return objeto;
+            } else {
+                return null;
+            }
+        } else {
+            livro.setStatusLivro("Emprestado");
             this.emprestimos.add(objeto);
             return objeto;
         }
@@ -45,7 +63,8 @@ public class EmprestimoDAOlist implements EmprestimoDAO{
     public Emprestimo buscarporId(Integer id) {
         return null;
     }
-    public List<Emprestimo> buscarEmprestimosporId(Integer id){
+
+    public List<Emprestimo> buscarEmprestimosporId(Integer id) {
         List<Emprestimo> listEmprestimos = new ArrayList<>();
         for (Emprestimo emprestimo : this.emprestimos) {
             if (emprestimo.getId().equals(id)) {
@@ -54,16 +73,46 @@ public class EmprestimoDAOlist implements EmprestimoDAO{
         }
         return listEmprestimos;
     }
-    public Integer calcularMulta(Emprestimo objeto){
+
+    public Integer calcularMulta(Emprestimo objeto) {
+        if (objeto != null) {
+            LocalDate dataDevolucao = objeto.getDataDevolucao();
+            LocalDate entregaFeita = LocalDate.now();
+
+            Integer diasAtraso = Math.toIntExact(ChronoUnit.DAYS.between(dataDevolucao, entregaFeita));
+            if (diasAtraso > 0) {
+                Integer valorMulta = diasAtraso * 2;
+                return valorMulta;
+            }
+        }
+        return 0;
+    }
+
+    public Emprestimo renovarEmprestimo(Emprestimo objeto) {
         return null;
     }
-    public Emprestimo renovarEmprestimo(Emprestimo objeto){
-     return null;
-    }
-    public Emprestimo registrarDevolucao(Emprestimo objeto){
+
+    public Emprestimo registrarDevolucao(Emprestimo objeto) {
+        if (objeto != null) {
+            if (objeto.getLivro().getStatusLivro().equals("Emprestado")) {
+                objeto.getLivro().setStatusLivro("Disponivel");
+                if (objeto.getLivro().proxReserva() != null){
+                    Emprestimo proximo = objeto.getLivro().proxReserva();
+
+                    objeto.getLivro().tiraFilareserva();
+                    return proximo;
+                }
+            }
+        }
         return null;
     }
-    public Emprestimo realizarReserva(Emprestimo objeto){
+
+    public Emprestimo realizarReserva(Emprestimo objeto) {
+        if (objeto!=null){
+                objeto.getLivro().addReserva(objeto);
+                return objeto;
+        }
         return null;
     }
+    
 }
