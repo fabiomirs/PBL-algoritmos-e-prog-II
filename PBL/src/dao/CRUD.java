beb73a53aa9f7bc4eb1e3b35be37fe8c8a674192
fabiomirs <fabiomirs@users.noreply.github.com>
2023-09-17 09:14:11 -0300

    :construction_worker: feat: Generics para as exceções.

--- PBL/src/dao/CRUD.java
+++ PBL/src/dao/CRUD.java
@@ -2,10 +2,10 @@ package dao;
 
 import java.util.List;
 
-public interface CRUD<T>{
-    public T create(T obj);
+public interface CRUD<T, E extends Exception>{
+    public T create(T obj) throws E;
     public List<T> read();
-    public T update(T obj);
-    public void delete(T obj);
-    public T buscarporId(Integer id);
+    public T update(T obj) throws E;
+    public void delete(T obj) throws E;
+    public T buscarporId(Integer id) throws E;
 }
