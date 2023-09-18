833a8edb5f675b2a24bc3a7f5bc0f844c10f504b
fabiomirs <fabiomirs@users.noreply.github.com>
2023-09-18 15:21:55 -0300

    :snowflake: feat: Adição da declaração do método deleteMany ao CRUD e mudança da exceção associada ao método create.

--- PBL/src/dao/CRUD.java
+++ PBL/src/dao/CRUD.java
@@ -3,9 +3,11 @@ package dao;
 import java.util.List;
 
 public interface CRUD<T, E extends Exception>{
-    public T create(T obj) throws E;
+    public T create(T obj);
     public List<T> read();
     public T update(T obj) throws E;
     public void delete(T obj) throws E;
+    public void deleteMany();
+
     public T buscarporId(Integer id) throws E;
 }
