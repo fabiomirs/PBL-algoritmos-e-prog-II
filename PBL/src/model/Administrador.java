4bdd4b427a89cd9b8d031a05db58351390e40b54
fabiomirs <fabiomirs@users.noreply.github.com>
2023-09-19 14:21:03 -0300

    :construction_worker: feat: Adição do construtor com id na classe Administrador.

--- PBL/src/model/Administrador.java
+++ PBL/src/model/Administrador.java
@@ -5,5 +5,8 @@ public class Administrador extends Bibliotecario {
     public Administrador(String nome, String cargo, Integer senhaAcesso) {
         super(nome, cargo, senhaAcesso);
     }
+    public Administrador(String nome, String cargo, Integer senhaAcesso, int id){
+        super(nome, cargo, senhaAcesso, id);
+    }
 
 }
