4cb93f3d98475e8592b64ffd9a20210b236c0cc6
fabiomirs <fabiomirs@users.noreply.github.com>
2023-09-23 14:20:20 -0300

    :snowflake: feat: Add dos métodos relacionados ao relatório na classe EmprestimoDAOlist.

--- PBL/src/dao/emprestimo/EmprestimoDAO.java
+++ PBL/src/dao/emprestimo/EmprestimoDAO.java
@@ -3,11 +3,17 @@ package dao.emprestimo;
 import dao.CRUD;
 import exceptions.EmprestimoException;
 import model.Emprestimo;
+import model.Livro;
 
 import java.util.List;
 
 public interface EmprestimoDAO extends CRUD<Emprestimo, EmprestimoException> {
     public Emprestimo criarEmprestimo(Emprestimo objeto) throws EmprestimoException;
+    public int numeroLivrosEmprestados();
+    public int numeroLivrosReservados();
+    public int numeroLivrosAtrasados();
+    public List<Emprestimo> historicoEmprestimosUsuario(int idUsuario);
+    public List<Livro> livrosMaisPopulares();
 
     public List<Emprestimo> buscarEmprestimosporId(Integer id) throws EmprestimoException;
     public Integer calcularMulta(Emprestimo objeto) throws EmprestimoException;
