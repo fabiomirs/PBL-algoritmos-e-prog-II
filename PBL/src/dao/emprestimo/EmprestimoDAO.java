0d30215ba3b72398b2664ff613216df2d3d91cd5
fabiomirs <fabiomirs@users.noreply.github.com>
2023-09-24 18:59:27 -0300

    :snowflake: feat: Algumas mudanças realizadas na interface do Emprestimo.

--- PBL/src/dao/emprestimo/EmprestimoDAO.java
+++ PBL/src/dao/emprestimo/EmprestimoDAO.java
@@ -9,15 +9,13 @@ import java.util.List;
 
 public interface EmprestimoDAO extends CRUD<Emprestimo, EmprestimoException> {
     public Emprestimo criarEmprestimo(Emprestimo objeto) throws EmprestimoException;
-    public int numeroLivrosEmprestados();
-    public int numeroLivrosReservados();
-    public int numeroLivrosAtrasados();
-    public List<Emprestimo> historicoEmprestimosUsuario(int idUsuario);
-    public List<Livro> livrosMaisPopulares();
-
-    public List<Emprestimo> buscarEmprestimosporId(Integer id) throws EmprestimoException;
+    public List<Emprestimo> LivrosEmprestados() throws EmprestimoException;
+    public List<Emprestimo> emprestimosAtrasados() throws EmprestimoException;
+    public List<Emprestimo> historicoEmprestimosUsuario(Integer idUsuario) throws EmprestimoException;
+    public List<Livro> livrosMaisPopulares() throws EmprestimoException;
+    //public List<Emprestimo> buscarEmprestimosporId(Integer id) throws EmprestimoException;
     public Integer calcularMulta(Emprestimo objeto) throws EmprestimoException;
     public Emprestimo renovarEmprestimo(Emprestimo objeto) throws EmprestimoException;
-    public Emprestimo registrarDevolucao(Emprestimo objeto) throws EmprestimoException;
-    public Emprestimo realizarReserva(Emprestimo objeto) throws EmprestimoException;
+    public void registrarDevolucao(Emprestimo objeto) throws EmprestimoException;
+
     }
