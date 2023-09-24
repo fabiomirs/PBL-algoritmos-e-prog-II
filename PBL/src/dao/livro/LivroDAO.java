fad12fbb5aa04d136d9c332ce64cccfda3aa55a2
fabiomirs <fabiomirs@users.noreply.github.com>
2023-09-24 18:51:43 -0300

    :construction_worker: feat: Adicionando métodos na interface do livro.
    - método de realizarReserva
    - método de verificar livrosComReserva.

--- PBL/src/dao/livro/LivroDAO.java
+++ PBL/src/dao/livro/LivroDAO.java
@@ -1,8 +1,11 @@
 package dao.livro;
 
 import dao.CRUD;
+import exceptions.EmprestimoException;
 import exceptions.LivroException;
+import model.Emprestimo;
 import model.Livro;
+import model.Usuario;
 
 import java.util.List;
 
@@ -11,6 +14,8 @@ public interface LivroDAO extends CRUD<Livro, LivroException> {
     public List<Livro> buscarporTitulo(String titulo) throws LivroException;
     public List<Livro> buscarPorIsbn(Integer Isbn) throws LivroException;
     public List<Livro> buscarPorCategoria(String categoria) throws LivroException;
+    public Livro realizarReserva(Usuario objeto, Livro obj) throws LivroException;
+    public List<Livro> livrosComReserva() throws LivroException;
 
 
 }
