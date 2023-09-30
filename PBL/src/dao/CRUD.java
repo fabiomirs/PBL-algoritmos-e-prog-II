f0ddf242dca849535f700946ca5d9d8a4761b5ed
fabiomirs <fabiomirs@users.noreply.github.com>
2023-09-30 08:58:30 -0300

    :pencil: feat: Adicionado os comentários javadoc para as classes CRUD e DAO.

--- PBL/src/dao/CRUD.java
+++ PBL/src/dao/CRUD.java
@@ -1,13 +1,51 @@
 package dao;
-
 import java.util.List;
 
+/**
+ * A interface CRUD determina operações principais para manipular dados.
+ * @param <T> O tipo de objeto que é manipulado.
+ * @param <E> A exceção que pode ser gerada.
+ */
 public interface CRUD<T, E extends Exception>{
+    /**
+     * Cria um novo objeto.
+     * @param obj O objeto a ser criado.
+     * @return O objeto já criado.
+     */
     public T create(T obj);
+
+    /**
+     * Lê e retorna uma lista de todos os objetos armazenados.
+     * @return Uma lista de todos os objetos.
+     */
     public List<T> read();
+
+    /**
+     * Atualiza um objeto existente no armazenamento.
+     *
+     * @param obj O objeto a ser atualizado.
+     * @return O objeto atualizado.
+     * @throws E Uma exceção que pode ser lançada durante a operação de atualização.
+     */
     public T update(T obj) throws E;
+
+    /**
+     * Deleta um objeto existente.
+     * @param obj O objeto a ser excluído do armazenamento.
+     * @throws E Uma exceção que pode ser lançada durante a operação de exclusão.
+     */
     public void delete(T obj) throws E;
+
+    /**
+     * Deleta todos os objetos armazenados.
+     */
     public void deleteMany();
 
+    /**
+     * Busca um objeto pelo seu ID.
+     * @param id O ID do objeto a ser buscado.
+     * @return O objeto encontrado com o ID especificado.
+     * @throws E Uma exceção que pode ser lançada se o objeto não for encontrado.
+     */
     public T buscarporId(Integer id) throws E;
 }
--- PBL/src/dao/DAO.java
+++ PBL/src/dao/DAO.java
@@ -1,5 +1,4 @@
 package dao;
-
 import dao.administrador.AdmDAO;
 import dao.administrador.AdmDAOlist;
 import dao.bibliotecario.BibliotecarioDAO;
@@ -11,8 +10,16 @@ import dao.livro.LivroDAOlist;
 import dao.usuario.UsuarioDAO;
 import dao.usuario.UsuarioDAOlist;
 
+/**
+ * A classe DAO é responsável por fornecer o acesso às implementações das interfaces DAO do sistema.
+ */
 public class DAO {
     private static LivroDAO livroDAO;
+
+    /**
+     * Obtém a instância do LivroDAO para ter acesso aos métodos lá implementados.
+     * @return A instância de LivroDAO.
+     */
     public static LivroDAO getLivroDAO() {
         if (livroDAO == null) {
             livroDAO = new LivroDAOlist();
@@ -21,6 +28,11 @@ public class DAO {
     }
 
     private static UsuarioDAO usuarioDAO;
+
+    /**
+     * Obtém a instância do UsuarioDAO para ter acesso aos métodos lá implementados.
+     * @return A instância de UsuarioDAO.
+     */
     public static UsuarioDAO getUsuarioDAO(){
         if (usuarioDAO == null){
             usuarioDAO = new UsuarioDAOlist();
@@ -29,7 +41,12 @@ public class DAO {
     }
 
     private static EmprestimoDAO emprestimoDAO;
-    public static EmprestimoDAO getEmprestimoDAODAO(){
+
+    /**
+     * Obtém a instância do EmprestimoDAO para ter acesso aos métodos lá implementados.
+     * @return A instância de EmprestimoDAO.
+     */
+    public static EmprestimoDAO getEmprestimoDAO(){
         if (emprestimoDAO == null){
             emprestimoDAO = new EmprestimoDAOlist();
         }
@@ -37,6 +54,11 @@ public class DAO {
     }
 
     private static BibliotecarioDAO bibliotecarioDAO;
+
+    /**
+     * Obtém a instância do BibliotecarioDAO para ter acesso aos métodos lá implementados.
+     * @return A instância de BibliotecarioDAO.
+     */
     public static BibliotecarioDAO getBibliotecarioDAO(){
         if (bibliotecarioDAO == null){
             bibliotecarioDAO = new BibliotecarioDAOlist();
@@ -45,6 +67,11 @@ public class DAO {
     }
 
     private static AdmDAO admDAO;
+
+    /**
+     * Obtém a instância do AdmDAO para ter acesso aos métodos lá implementados.
+     * @return A instância de AdmDAO.
+     */
     public static AdmDAO getAdmDAO(){
         if (admDAO == null){
             admDAO = new AdmDAOlist();
