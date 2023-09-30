6d1a353f6d2e583aee5e55481cbbe88e6c94ac8d
fabiomirs <fabiomirs@users.noreply.github.com>
2023-09-30 09:09:27 -0300

    :snowflake: feat: Adicionado o método de realizar reserva a classe DAO do livro.
    - Também adicionado os comentários javadoc.

--- PBL/src/dao/livro/LivroDAOlist.java
+++ PBL/src/dao/livro/LivroDAOlist.java
@@ -1,37 +1,62 @@
 package dao.livro;
-
-
 import exceptions.LivroException;
-
 import model.Livro;
 import model.Usuario;
-
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A classe LivroDAOlist implementa os métodos relacionados a interface LivroDAO.
+ */
 public class LivroDAOlist implements LivroDAO {
     private List<Livro> Livros;
     private int proximoID;
 
+    /**
+     * Método responsável por criar a lista de livros e determinar
+     * o id inicial como 0.
+     */
     public LivroDAOlist() {
         this.Livros = new ArrayList<>();
         this.proximoID = 0;
     }
 
+    /**
+     * @return Retorna o id que será utilizado para a criação do livro.
+     */
     private int getProximoID() {
         return this.proximoID++;
     }
 
+    /**
+     * Método responsável por atribuir um identificador ao objeto Livro e
+     * adicioná-lo a lista de livros.
+     * @param objeto Objeto Livro que será criado.
+     * @return O objeto criado.
+     */
+    @Override
     public Livro create(Livro objeto) {
         objeto.setId(this.getProximoID());
         this.Livros.add(objeto);
         return objeto;
     }
 
+    /**
+     * Método que retorna a lista de livros.
+     * @return A lista de livros existente.
+     */
+    @Override
     public List<Livro> read() {
         return Livros;
     }
 
+    /**
+     * Método utilizado para atualizar um objeto Livro.
+     * @param objeto Objeto Livro que vai ser atualizado.
+     * @return O objeto atualizado.
+     * @throws LivroException Exceção que pode ser lançada, caso o livro não exista.
+     */
+    @Override
     public Livro update(Livro objeto) throws LivroException {
         int index = this.Livros.indexOf(objeto);
         if (index != -1) {
@@ -42,6 +67,11 @@ public class LivroDAOlist implements LivroDAO {
         }
     }
 
+    /**
+     * Método utilizado para deletar um objeto Livro da lista existente.
+     * @param objeto Objeto que vai ser deletado.
+     * @throws LivroException Exceção que pode ser lançada, caso o livro não exista na lista.
+     */
     public void delete(Livro objeto) throws LivroException {
         boolean remove = this.Livros.remove(objeto);
         if (!remove) {
@@ -49,12 +79,22 @@ public class LivroDAOlist implements LivroDAO {
         }
     }
 
+    /**
+     * Método utilizado para excluir a lista de livros e
+     * reinicializar o id como 0.
+     */
     @Override
     public void deleteMany() {
         this.Livros = new ArrayList<>();
         this.proximoID = 0;
     }
 
+    /**
+     * Método usado para buscar um livro por id.
+     * @param id O id a ser buscado.
+     * @return O objeto encontrado.
+     * @throws LivroException Exceção que pode ser lançada, caso o id não exista na lista.
+     */
     public Livro buscarporId(Integer id) throws LivroException {
         for (Livro Livro : this.Livros) {
             if (Livro.getId() == id) {
@@ -64,6 +104,13 @@ public class LivroDAOlist implements LivroDAO {
         throw new LivroException(LivroException.BUSCA_ID, null);
     }
 
+    /**
+     * Método usado para buscar um livro por título.
+     * @param titulo O título a ser buscado.
+     * @return Uma lista de livros com o título de interesse.
+     * @throws LivroException Exceção que pode ser lançada, caso um livro
+     * com o título buscado não exista na lista.
+     */
     public List<Livro> buscarporTitulo(String titulo) throws LivroException {
         List<Livro> listLivro = new ArrayList<Livro>();
         for (Livro livro : this.Livros) {
@@ -78,6 +125,13 @@ public class LivroDAOlist implements LivroDAO {
         }
     }
 
+    /**
+     * Método usado para buscar um livro por autor.
+     * @param autor O autor a ser buscado.
+     * @return Uma lista de livros com o autor de interesse.
+     * @throws LivroException Exceção que pode ser lançada, caso um livro
+     * com o autor buscado não exista na lista.
+     */
     public List<Livro> buscarporAutor(String autor) throws LivroException {
         List<Livro> listLivro = new ArrayList<Livro>();
         for (Livro livro : this.Livros) {
@@ -92,6 +146,13 @@ public class LivroDAOlist implements LivroDAO {
         }
     }
 
+    /**
+     * Método usado para buscar um livro por isbn.
+     * @param isbn O isbn a ser buscado.
+     * @return Uma lista de livros com o isbn de interesse.
+     * @throws LivroException Exceção que pode ser lançada, caso um livro
+     * com o isbn buscado não exista na lista.
+     */
     public List<Livro> buscarPorIsbn(Integer isbn) throws LivroException {
         List<Livro> listLivro = new ArrayList<>();
         for (Livro livro : this.Livros) {
@@ -106,6 +167,13 @@ public class LivroDAOlist implements LivroDAO {
         }
     }
 
+    /**
+     * Método usado para buscar um livro por categoria.
+     * @param categoria A categoria a ser buscado.
+     * @return Uma lista de livros com a categoria de interesse.
+     * @throws LivroException Exceção que pode ser lançada, caso um livro
+     * com a categoria buscado não exista na lista.
+     */
     public List<Livro> buscarPorCategoria(String categoria) throws LivroException {
         List<Livro> listLivro = new ArrayList<>();
         for (Livro livro : this.Livros) {
@@ -120,14 +188,32 @@ public class LivroDAOlist implements LivroDAO {
         }
     }
 
-    public Livro realizarReserva(Usuario objeto, Livro obj) throws LivroException {
-        if (objeto != null && objeto.getStatusConta().equals("Liberado")) {
+    /**
+     * Método utilizado para adicionar um usuário a fila de reservas de um livro.
+     * @param objeto O usuário que deseja reservar o livro.
+     * @param livroId O id do livro que deseja ser reservado.
+     * @return O livro que vai ser reservado.
+     * @throws LivroException Exceção que pode ser lançada caso um dos parametros
+     * seja nulo ou o usuário está bloqueado.
+     */
+    public Livro realizarReserva(Usuario objeto, Integer livroId) throws LivroException {
+        // Encontre o livro com base no ID fornecido
+        Livro obj = buscarporId(livroId);
+
+        if (obj != null && objeto != null && objeto.getStatusConta().equals("Liberado")) {
             obj.addReserva(objeto);
             return obj;
         }
+
         throw new LivroException(LivroException.RESERVA, obj);
     }
 
+    /**
+     * Metódo usado para buscar os livros com reserva.
+     * @return A lista de livros que estão reservados.
+     * @throws LivroException Exceção que pode ser lançada caso nenhum livro
+     * possua uma reserva.
+     */
     public List<Livro> livrosComReserva() throws LivroException {
         List<Livro> livrosComReserva = new ArrayList<>();
         for (Livro livro : Livros) {
