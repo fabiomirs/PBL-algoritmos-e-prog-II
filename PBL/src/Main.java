6b9646e7acd14ef6993ea3b3078d674ac0254657
fabiomirs <fabiomirs@users.noreply.github.com>
2023-10-01 09:44:53 -0300

    :warning: Comentando o que havia utilizado na main.

--- PBL/src/Main.java
+++ PBL/src/Main.java
@@ -4,29 +4,43 @@
 import dao.DAO;
 import dao.livro.LivroDAO;
 import dao.livro.LivroDAOlist;
+import model.Emprestimo;
 import model.Livro;
 import model.Usuario;
 
 public class Main {
     public static void main(String[] args) {
 
-        DAO.getLivroDAO().create(new Livro("teste","valoriza",456879,"zona norte","ocupado","fabio","00/00/2023"));
-        DAO.getLivroDAO().create(new Livro("teste","valggoriza",456879,"zonagg norte","ocupado","fabio","00/00/2023"));
-        DAO.getLivroDAO().create(new Livro("teggste","valoriza",456879,"zona norte","ocupado","fabio","00/00/2023"));
-        DAO.getLivroDAO().create(new Livro("teste","valoriza",456879,"zona norte","ocupado","fabggio","00/00/2023"));
+        //DAO.getLivroDAO().create(new Livro("teste","valoriza",456879,"zona norte","ocupado","fabio","00/00/2023"));
+        //DAO.getLivroDAO().create(new Livro("teste","valggoriza",456879,"zonagg norte","ocupado","fabio","00/00/2023"));
+        //DAO.getLivroDAO().create(new Livro("teggste","valoriza",456879,"zona norte","ocupado","fabio","00/00/2023"));
+        //DAO.getLivroDAO().create(new Livro("teste","valoriza",456879,"zona norte","ocupado","fabggio","00/00/2023"));
 
 
-        DAO.getLivroDAO().create(new Livro("Livro 1", "Editora 1", 123456, "Local 1", "Disponível", "Autor 1", "2022"));
-        DAO.getLivroDAO().create(new Livro("Livro 2", "Editora 2", 789012, "Local 2", "Emprestado", "Autor 2", "2023"));
-        DAO.getLivroDAO().create(new Livro("Livro 3", "Editora 3", 345678, "Local 3", "Disponível", "Autor 3", "2024"));
+        //DAO.getLivroDAO().create(new Livro("Livro 1", "Editora 1", 123456, "Local 1", "Disponível", "Autor 1", "2022"));
+       // DAO.getLivroDAO().create(new Livro("Livro 2", "Editora 2", 789012, "Local 2", "Emprestado", "Autor 2", "2023"));
+       // DAO.getLivroDAO().create(new Livro("Livro 3", "Editora 3", 345678, "Local 3", "Disponível", "Autor 3", "2024"));
 
 
+        //Livro livroatt = DAO.getLivroDAO().create(new Livro("teste","valggoriza",456879,"zonagg norte","ocupado","fabio","00/00/2023"));
+
+        //System.out.println(DAO.getLivroDAO().read());
+
+        //DAO.getUsuarioDAO().create(new Usuario("fabio", "tert", 456789123));
+
+        //Emprestimo teste = new Emprestimo(livroatt, DAO.getUsuarioDAO().read().get(0));
+
+       // livroatt.setTitulo("MUDANÇA");
+       // DAO.getLivroDAO().update(livroatt);
+
+        //System.out.println(DAO.getLivroDAO().read());
+
+       // System.out.println(teste.toString());
 
 
-
-        System.out.println(DAO.getLivroDAO().read());
-        System.out.println("\n\n");
-        System.out.println(DAO.getLivroDAO().buscarporIsbn(345678));
+        //System.out.println(DAO.getLivroDAO().read());
+        //System.out.println("\n\n");
+        //System.out.println(DAO.getLivroDAO().buscarPorIsbn(456879));
 
         //System.out.println(DAO.getLivroDAO().buscarporId(0));
 
