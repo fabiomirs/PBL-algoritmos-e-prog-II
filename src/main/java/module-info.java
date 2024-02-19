dd365e9eeac8b5730a35a54b9735c003e29ed066
fabiomirs <fabiomirs@users.noreply.github.com>
2024-02-18 23:50:32 -0300

    :snowflake: feat: Atualização do module-info para utilização do JAVA FX

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.pbl {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     opens com.example.pbl.model;
     opens com.example.pbl.dao.livro;
@@ -9,6 +10,9 @@ module com.example.pbl {
     opens com.example.pbl.dao.emprestimo;
     opens com.example.pbl.dao.bibliotecario;
 
-    opens com.example.pbl to javafx.fxml;
+    opens com.example.pbl to javafx.fxml, javafx.graphics;
     exports com.example.pbl;
+
+    opens com.example.pbl.controller to javafx.fxml, javafx.graphics;
+    exports com.example.pbl.controller;
 }
